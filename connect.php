0e58e095aef5db8477f826c261b59a31a088cd5e
Nikos Paschalis <nikospasxalis94@gmail.com>
2020-06-16 19:38:42 +0300

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
-    $host = "127.0.0.1";
-    $user = "nikosp";                     
+    $host = "";
+    $user = "";                     
     $pass = "";                                  
     $db = "fcb";                                  
                               
